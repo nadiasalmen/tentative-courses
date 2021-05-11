03d1976fadae20c00abddfbf201350185bee14e0
Nadia Salmen <nadiasalmen@gmail.com>
2021-05-11 00:37:43 -0300

    Add Student class validations

--- lib/student.rb
+++ lib/student.rb
@@ -2,9 +2,19 @@
 
 # this class represents a student
 class Student
-  def initialize(delivery_mode, level)
-    @delivery_mode = delivery_mode
-    @level = level
+  class InvalidValue < StandardError; end
+
+  attr_reader :delivery_mode, :level
+
+  DELIVERY_MODES = %w[Individual Group].freeze
+  LEVELS = %w[Beginner Pre-Intermediate Intermediate Upper-Intermediate Advanced].freeze
+
+  def initialize(attributes = {})
+    raise InvalidValue unless DELIVERY_MODES.include?(attributes[:delivery_mode])
+    raise InvalidValue unless LEVELS.include?(attributes[:level])
+
+    @delivery_mode = attributes[:delivery_mode]
+    @level = attributes[:level]
     @timeslots = []
     @allocated = false
   end
--- spec/student_test.rb
+++ spec/student_test.rb
@@ -1,4 +1,32 @@
 # frozen_string_literal: true
 
-require 'rspec'
 require_relative '../lib/student'
+
+shared_context 'student instances' do
+  before do
+    @student = Student.new(
+      delivery_mode: 'Individual',
+      level: 'Beginner'
+    )
+  end
+end
+
+describe Student do
+  include_context 'student instances'
+
+  it 'has a delivery_mode' do
+    expect(@student.delivery_mode).to eq('Individual')
+  end
+
+  it 'has a level' do
+    expect(@student.level).to eq('Beginner')
+  end
+
+  it 'raises InvalidValue if delivery_mode is wrong' do
+    expect { Student.new(delivery_mode: '', level: 'Beginner') }.to raise_error(Student::InvalidValue)
+  end
+
+  it 'raises InvalidValue if asked level is wrong' do
+    expect { Student.new(delivery_mode: 'Individual', level: '') }.to raise_error(Student::InvalidValue)
+  end
+end
