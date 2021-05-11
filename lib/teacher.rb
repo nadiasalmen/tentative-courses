b93e2b47abc9409a9e2f9d08112d9db0663ca3f4
Nadia Salmen <nadiasalmen@gmail.com>
2021-05-10 23:25:24 -0300

    Add Timeslot class

---
+++ lib/teacher.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# this class represents a student
+class Teacher
+  def initialize
+    @timeslots = []
+    @allocated = false
+  end
+end
---
+++ lib/timeslot.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# this class represents a student
+class Timeslot
+  def initialize(day, time)
+    @day = day
+    @time = time
+  end
+
+  # def show_timeslot
+  #   { day: @day, hour: @hour }
+  # end
+end
